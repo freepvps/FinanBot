d18f1c5ea012239c21525e06969afe039a04edc2
Максим <freepvps@gmail.com>
2016-03-12 22:08:07 +0300

    Добавил недостающие классы*

---
+++ Finanbot/Core/Helpers/UserTimeParser.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finanbot.Core.Helpers
+{
+    public class UserTimeParser
+    {
+        private static int GetTime(string token)
+        {
+            if (string.IsNullOrEmpty(token))
+            {
+                return 0;
+            }
+            // с, м, ч, д
+            // секунда
+            // минута
+            // час
+            // день
+            var ch = token[0];
+            switch(ch)
+            {
+                case 'с': return 1;
+                case 'м': return 60;
+                case 'ч': return 60 * 60;
+                case 'д': return 60 * 60 * 24;
+                case 'н': return 60 * 60 * 24 * 7;
+            }
+            return 0;
+        }
+        public static int ParseTime(string s, int defaultTime = 60)
+        {
+            s = s + " ";
+
+            var tokensList = new List<string>();
+            var i = 0;
+
+            for (var j = 0; j < s.Length; j++)
+            {
+                var isdg = char.IsDigit(s[j]);
+                var islt = char.IsLetter(s[j]);
+
+                var lisdg = char.IsDigit(s[i]);
+                var lislt = char.IsLetter(s[i]);
+                var length = j - i;
+
+                if (!isdg && !islt)
+                {
+                    if (length > 0)
+                    {
+                        tokensList.Add(s.Substring(i, length));
+                    }
+                    i = j + 1;
+                    continue;
+                }
+                if (lislt != islt || lisdg != isdg)
+                {
+                    tokensList.Add(s.Substring(i, length));
+                    i = j;
+                }
+            }
+            var ans = 0;
+            var tokens = new Queue<string>(tokensList);
+            while (tokens.Count > 0)
+            {
+                var token = tokens.Dequeue();
+                var time = 0;
+                if (int.TryParse(token, out time))
+                {
+                    var mod = defaultTime;
+                    if (tokens.Count > 0)
+                    {
+                        mod = GetTime(tokens.Peek());
+                        if (mod != 0)
+                        {
+                            tokens.Dequeue();
+                        }
+                        else
+                        {
+                            mod = defaultTime;
+                        }
+                    }
+                    ans += time * mod;
+                }
+                else
+                {
+                    time += GetTime(token);
+                }
+            }
+            return ans;
+        }
+        public static string SecondsToString(int seconds)
+        {
+            var d = new Dictionary<string, int>
+            {
+                { "сек.", 60 },
+                { "мин.", 60 },
+                { "ч.", 60 },
+                { "дн.", 24 },
+                { "нед.", 7 }
+            };
+
+            var ans = string.Empty;
+            foreach (var x in d)
+            {
+                var t = seconds % x.Value;
+                seconds /= x.Value;
+
+                if (t != 0)
+                {
+                    if (!string.IsNullOrWhiteSpace(ans)) ans = " " + ans;
+                    ans = string.Format("{0} {1}", t, x.Key) + ans;
+                }
+            }
+            if (string.IsNullOrWhiteSpace(ans)) return "0 сек";
+            return ans;
+        }
+    }
+}
---
+++ Finanbot/Core/Plugins/PulsePlugin.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Finanbot.Core.Helpers;
+using Telegram.Bot.Types;
+
+namespace Finanbot.Core.Plugins
+{
+    public class PulsePlugin : Plugin
+    {
+        public const int MinimumTime = 60;
+
+        public int PulseTime { get; set; }
+
+        public PulsePlugin()
+        {
+            PulseTime = MinimumTime;
+        }
+        public override bool CanRun
+        {
+            get
+            {
+                return true;
+            }
+        }
+        public override string Description
+        {
+            get
+            {
+                return "Настройка частоты обновлений";
+            }
+        }
+        public override string PluginName
+        {
+            get
+            {
+                return "pulse";
+            }
+        }
+        public override string UserPluginName
+        {
+            get
+            {
+                return "Pulse";
+            }
+        }
+
+        public DateTime LastPulse { get; set; }
+        public void SafePulse(Session session, bool force)
+        {
+            var time = DateTime.Now;
+            var dt = time - LastPulse;
+            if (force || dt.TotalSeconds > PulseTime)
+            {
+                LastPulse = DateTime.Now;
+                foreach(var plugin in session.Plugins)
+                {
+                    plugin.Value.Pulse(session);
+                }
+            }
+        }
+
+        public override int GetPriority(Message query)
+        {
+            if (query.Type == MessageType.TextMessage)
+            {
+                var keywords = new string[][]
+                {
+                    new string[] { "частот", "период", "периуд" },
+                    new string[] { "обновл", "пульс", "уведомл" }
+                };
+                var price = Ext.PriceCalc(query.Text, keywords);
+                return price * 50;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        public override bool Query(Session session, Message query)
+        {
+            SendAnswer(session, "Уведомления обновляются каждые " + UserTimeParser.SecondsToString(PulseTime));
+            return true;
+        }
+
+        public override void Help(Session session)
+        {
+            Send(session, "Как часто вы хотите получать обновления? (выберите из списка или укажите вручную)", new ReplyQuadreKeyboard(true, "1 день", "12 часов", "4 часа", "1 час"));
+        }
+        public override void Command(Session session, Message message)
+        {
+            if (message.Type == MessageType.TextMessage)
+            {
+                var time = UserTimeParser.ParseTime(message.Text, 60);
+                var nt = Math.Max(time, MinimumTime);
+                if (time < MinimumTime)
+                {
+                    Send(session, "Указанная вами частота обновления слишком маленькая:( Минимальная частота - " + UserTimeParser.SecondsToString(MinimumTime));
+                    Help(session);
+                    return;
+                }
+                else
+                {
+                    PulseTime = nt;
+                    Send(session, "Обновления будут приходить каждые " + UserTimeParser.SecondsToString(nt));
+                    session.StopPlugin(this);
+                    return;
+                }
+            }
+            else
+            {
+                Help(session);
+            }
+        }
+    }
+}
